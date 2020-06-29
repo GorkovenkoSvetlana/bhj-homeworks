d60d01babf022284b4e8cdb9382186530226906d
Svetlana Gorkovenko <lipa-@mail.ru>
2020-06-29 19:36:05 +0300

    Third commit

---
+++ element-search/popups/task.js
@@ -0,0 +1,23 @@
+// Всплывает окно с окошком Сделать хорошо
+ //Если нажали кнопку Сделать хорошо, то меняется на кнопку Хорошо сделано и можно закрыть
+ // Иначе невозможно закрыть
+let modalMain = document.getElementById('modal_main');
+let modalClose = document.querySelectorAll('.modal__close');
+let btnDanger = document.getElementsByClassName('btn_danger');
+
+document.querySelector('.modal').classList.add('modal_active');
+
+function closeModal () {
+
+   if(btnDanger.onclick) {
+     
+   }
+ 
+ 
+}
+
+
+
+
+
+ 
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,17 +1,11 @@
 const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
+const holes = document.querySelectorAll('.hole');
 
-function getHole(index) {
-  let hole = document.getElementById('hole') + index;
-  return hole;
-}
+  for (let i = 0; i < holes.length; i++) {
 
-for (let i = 1; i < 10; i++) {
-
-  let hole = getHole(i);
-   
-   hole.onclick = function() {
-      if (hole.className.includes('hole_has-mole')) {
+   holes[i].onclick = function() {
+      if (holes[i].className.includes('hole_has-mole')) {
         dead.textContent ++;
     } else {
       lost.textContent ++;
@@ -24,9 +18,9 @@ for (let i = 1; i < 10; i++) {
   } else if (lost.textContent == 5) {
     alert('Вы проиграли!');
     reset();
- };
-}
-}
+    };
+   }
+  }
 
 function reset() {
   dead.textContent = 0;
